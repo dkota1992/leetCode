c18eb191455118953786bf1d1df4394ce9f31f3f
Deepak Kota <dkota1992@gmail.com>
2017-08-01 21:18:35 -0400

    Update twonumbers.py

--- LICENSE.md/twonumbers.py
+++ LICENSE.md/twonumbers.py
@@ -1,15 +1,28 @@
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-class LinkedList(object):
-    def __init__(self):
-        self.current = ListNode(None)
+class Solution(object):
     
-    def addNode(self,value):
-        if self.current.val = None:
-            self.current.val = value
-        else:
-            self.current.next = ListNode(value)
-            self.current.val = ListNode(value).val
+    def __init__(self,array,target):
+        self.array = array
+        self.target = target
+        self.dict = self.convertdict()
+        
+    def convertdict(self):
+        self.dict = {}
+        for i,j in enumerate(self.array):
+            self.dict[j] = i
+        return self.dict
+            
+        
+    def twoSum(self):
+        for i,j in enumerate(self.array):
+            if (self.target-j) in self.dict:
+                return [self.dict[self.target-j],i]
+
+def main():
+    a = Solution(eval(input()),int(input()))
+    print(a.twoSum())
+    print(a.dict)
+            
+            
+if __name__ == '__main__':
+    main()
+    
